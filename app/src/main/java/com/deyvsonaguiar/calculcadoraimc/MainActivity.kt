422fda481b1af7324ad663cfdfb23648568ff6da
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2021-03-12 07:43:38 -0400

    Definindo Linear Layout e elementos

--- app/src/main/java/com/deyvsonaguiar/calculcadoraimc/MainActivity.kt
+++ app/src/main/java/com/deyvsonaguiar/calculcadoraimc/MainActivity.kt
@@ -2,10 +2,42 @@ package com.deyvsonaguiar.calculcadoraimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.w("lifeCycle", "CREATE - estou criando a tela!")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.w("lifeCycle", "START - deixei a tela visível para você!")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.w("lifeCycle", "RESUME - agora você pode interagir com a tela!")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.w("lifeCycle", "PAUSE - a tela perdeu o foco, você não pode mais interagir!")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.w("lifeCycle", "STOP - a tela não está mais visível mas ainda existe!")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.w("lifeCycle", "RESTART - a tela está retomando o foco!")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.w("lifeCycle", "DESTROY - a tela foi destruída!")
     }
 }
